4f278c70dac9b7ea5055c6c36d76be5b8719e155
Anton Beznos <abeznos@yandex.ru>
2024-10-20 03:15:36 +0300

    main.js

--- src/main.js
+++ src/main.js
@@ -11,10 +11,7 @@ if (window.visualViewport) {
       document.body.style.height = window.visualViewport.height + 'px';
     });
   }
-  // This will ensure user never overscroll the page
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 0) window.scrollTo(0, 0);
-  });
+
 
 const app = createApp(App)
 
