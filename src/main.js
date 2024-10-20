4b86e48b0f11b906327130f07928a862d1158392
Anton Beznos <abeznos@yandex.ru>
2024-10-20 03:14:01 +0300

    main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,16 @@ import App from '@/App.vue'
 import store from '@/store';
 import router from '@/router'
 
+if (window.visualViewport) {
+    window.visualViewport.addEventListener('resize', () => {
+      document.body.style.height = window.visualViewport.height + 'px';
+    });
+  }
+  // This will ensure user never overscroll the page
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) window.scrollTo(0, 0);
+  });
+
 const app = createApp(App)
 
 app.use(store)
