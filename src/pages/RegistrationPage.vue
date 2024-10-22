44908c0e4d3ee3d329ece2359f436cf2c5776932
Anton Beznos <abeznos@yandex.ru>
2024-10-23 01:27:38 +0300

    reg

--- src/pages/RegistrationPage.vue
+++ src/pages/RegistrationPage.vue
@@ -6,101 +6,11 @@
         <v-row>
             <v-col class="v-col-12 d-flex flex-column justify-center align-center">
                 <h2 class="pb-6 pt-6">Анкета регистрации</h2>
-                <v-form class="w-100" validate-on="submit" ref="form" @submit.prevent>
-                    <v-text-field
-                        v-if="getFormFields.nameField.display"
-                        class="pb-text-field rounded-lg rounded-xl"
-                        v-model="userData.name"
-                        autofocus
-                        variant="outlined"
-                        :label="getFormFields.nameField.label"
-                        inputmode="text"
-                        :placeholder="getFormFields.nameField.placeholder"
-                        :rules="getRules(getFormFields.nameField.rules)"
-                    ></v-text-field>
-                    <v-text-field
-                        v-if="getFormFields.surnameField.display"
-                        class="pb-text-field"
-                        v-model="userData.surname"
-                        variant="outlined"
-                        :label="getFormFields.surnameField.label"
-                        density="compact"
-                        :placeholder="getFormFields.surnameField.placeholder"
-                        :rules="getRules(getFormFields.surnameField.rules)"
-                    ></v-text-field>
-                    <v-text-field
-                        v-if="getFormFields.middleName.display"
-                        class="pb-text-field"
-                        v-model="userData.middle_name"
-                        variant="outlined"
-                        :label="getFormFields.middleName.label"
-                        density="compact"
-                        :placeholder="getFormFields.middleName.placeholder"
-                        :rules="getRules(getFormFields.middleName.rules)"
-                    ></v-text-field>
-                    <v-radio-group
-                        v-if="getFormFields.gender.display"
-                        v-model="userData.gender"
-                        inline
-                        :rules="getRules(getFormFields.gender.rules)"
-                    >
-                        <v-radio label="Мужчина" value="male" color="var(--primary-color)"></v-radio>
-                        <v-radio label="Женщина" value="female" color="var(--primary-color)"></v-radio>
-                    </v-radio-group>
-                    <v-text-field
-                        v-if="getFormFields.birthDate.display"
-                        class="pb-text-field"
-                        v-model="userData.birth_date"
-                        variant="outlined"
-                        :label="getFormFields.birthDate.label"
-                        density="compact"
-                        inputmode="numeric"
-                        :placeholder="getFormFields.birthDate.placeholder"
-                        :rules="getRules(getFormFields.birthDate.rules)"
-                    ></v-text-field>
-                    <v-text-field
-                        v-if="getFormFields.email.display"
-                        class="pb-text-field"
-                        v-model="userData.email"
-                        variant="outlined"
-                        :label="getFormFields.email.label"
-                        density="compact"
-                        inputmode="email"
-                        :placeholder="getFormFields.email.placeholder"
-                        :rules="getRules(getFormFields.email.rules)"
-                    ></v-text-field>
-                    <v-select
-                        v-if="getFormFields.city.display"
-                        class="pb-text-field"
-                        v-model="userData.city"
-                        variant="outlined"
-                        :items="getFormFields.city.items"
-                        :label="getFormFields.city.label"
-                        density="compact"
-                        :rules="getRules(getFormFields.city.rules)"
-                    ></v-select>
-                    <v-checkbox color="deep-purple-accent-4">
-                        <template v-slot:label>
-                            <a
-                                href="https://vuetifyjs.com"
-                                target="_blank"
-                                @click.stop
-                            >
-                                Хочу получать реклманые сообщения
-                            </a>
-                        </template>
-                    </v-checkbox>
-                    <VBtn
-                        class="mt-2"
-                        block
-                        size="large"
-                        variant="flat"
-                        type="submit"
-                        :loading="loading"
-                        @click="sendForm"
-                        text="Далее"
-                    ></VBtn>
-                </v-form>
+                <form class="d-flex flex-column ga-8 w-100">
+                    <input class="w-100 border-sm" type="text">
+                    <input class="w-100 border-sm" type="text">
+                    <input class="w-100 border-sm" type="text">
+                </form>
             </v-col>
         </v-row>
     </v-container>
