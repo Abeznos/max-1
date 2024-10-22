fa3445e427bcb4224d75c12b99d9e957b2740756
Anton Beznos <abeznos@yandex.ru>
2024-10-23 01:39:06 +0300

    reg

--- src/pages/RegistrationPage.vue
+++ src/pages/RegistrationPage.vue
@@ -1,7 +1,216 @@
 <template>
-<input type="text" class="w-100 border-sm">
-<input type="text" class="w-100 border-sm">
-<input type="text" class="w-100 border-sm">
+    <v-container
+        v-if="!personaldataSend"
+        class="h-100"
+    >
+        <v-row>
+            <v-col class="v-col-12 d-flex flex-column justify-center align-center">
+                <h2 class="pb-6 pt-6">Анкета регистрации1</h2>
+                <v-form class="w-100">
+                    <v-text-field
+                        v-if="getFormFields.nameField.display"
+                        class="pb-text-field rounded-lg rounded-xl"
+                        v-model="userData.name"
+                        autofocus
+                        variant="outlined"
+                        :label="getFormFields.nameField.label"
+                        inputmode="text"
+                        :placeholder="getFormFields.nameField.placeholder"
+                        :rules="getRules(getFormFields.nameField.rules)"
+                    ></v-text-field>
+                    <v-text-field
+                        v-if="getFormFields.surnameField.display"
+                        class="pb-text-field"
+                        v-model="userData.surname"
+                        variant="outlined"
+                        :label="getFormFields.surnameField.label"
+                        density="compact"
+                        :placeholder="getFormFields.surnameField.placeholder"
+                        :rules="getRules(getFormFields.surnameField.rules)"
+                    ></v-text-field>
+                    <v-text-field
+                        v-if="getFormFields.middleName.display"
+                        class="pb-text-field"
+                        v-model="userData.middle_name"
+                        variant="outlined"
+                        :label="getFormFields.middleName.label"
+                        density="compact"
+                        :placeholder="getFormFields.middleName.placeholder"
+                        :rules="getRules(getFormFields.middleName.rules)"
+                    ></v-text-field>
+                    <v-radio-group
+                        v-if="getFormFields.gender.display"
+                        v-model="userData.gender"
+                        inline
+                        :rules="getRules(getFormFields.gender.rules)"
+                    >
+                        <v-radio label="Мужчина" value="male" color="var(--primary-color)"></v-radio>
+                        <v-radio label="Женщина" value="female" color="var(--primary-color)"></v-radio>
+                    </v-radio-group>
+                    <v-text-field
+                        v-if="getFormFields.birthDate.display"
+                        class="pb-text-field"
+                        v-model="userData.birth_date"
+                        variant="outlined"
+                        :label="getFormFields.birthDate.label"
+                        density="compact"
+                        inputmode="numeric"
+                        :placeholder="getFormFields.birthDate.placeholder"
+                        :rules="getRules(getFormFields.birthDate.rules)"
+                    ></v-text-field>
+                    <v-text-field
+                        v-if="getFormFields.email.display"
+                        class="pb-text-field"
+                        v-model="userData.email"
+                        variant="outlined"
+                        :label="getFormFields.email.label"
+                        density="compact"
+                        inputmode="email"
+                        :placeholder="getFormFields.email.placeholder"
+                        :rules="getRules(getFormFields.email.rules)"
+                    ></v-text-field>
+                    <v-select
+                        v-if="getFormFields.city.display"
+                        class="pb-text-field"
+                        v-model="userData.city"
+                        variant="outlined"
+                        :items="getFormFields.city.items"
+                        :label="getFormFields.city.label"
+                        density="compact"
+                        :rules="getRules(getFormFields.city.rules)"
+                    ></v-select>
+                    <v-checkbox color="deep-purple-accent-4">
+                        <template v-slot:label>
+                            <a
+                                href="https://vuetifyjs.com"
+                                target="_blank"
+                                @click.stop
+                            >
+                                Хочу получать реклманые сообщения
+                            </a>
+                        </template>
+                    </v-checkbox>
+                    <VBtn
+                        class="mt-2"
+                        block
+                        size="large"
+                        variant="flat"
+                        type="submit"
+                        :loading="loading"
+                        @click="sendForm"
+                        text="Далее"
+                    ></VBtn>
+                </v-form>
+            </v-col>
+        </v-row>
+    </v-container>
+
+    <div v-else>
+        <v-container
+            v-if="!showImportantDates"
+            class="d-flex flex-column justify-center"
+        >
+            <v-layout
+                class="alert w-100 h-100"
+            >
+                <v-col class="v-col-12 d-flex flex-column justify-center">
+                    <v-card class="pb-alert-card pa-4" variant="flat">
+                        <v-card-text>
+                            <p>Заполните даты важных событий или дни рождения близких людей, и&nbsp;перед праздником мы&nbsp;пришлем вам напоминание и&nbsp;подарок.</p>
+                        </v-card-text>
+                        <v-card-actions class=" w-100 d-flex flex-row align-center justify-center">
+                            <v-col>
+                                <VBtnOutline
+                                    class="mt-2 pb-text-bt"
+                                    block
+                                    size="large"
+                                    variant="flat"
+                                    type="submit"
+                                    :loading="loading"
+                                    @click="$router.push(`/${getBotId}`)"
+                                    text="Не сейчас"
+                                ></VBtnOutline>
+                            </v-col>
+                            <v-col>
+                                <VBtn
+                                    class="mt-2 pb-primary-bt"
+                                    block
+                                    size="large"
+                                    variant="flat"
+                                    type="submit"
+                                    :loading="loading"
+                                    @click="showImportantDates = !showImportantDates"
+                                    text="Заполнить"
+                                ></VBtn>
+                            </v-col>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-layout>
+        </v-container>
+        <v-container
+            v-else
+            class="d-flex flex-column"
+        >
+            <v-form class="w-100 d-flex flex-column justify-start ga-4" validate-on="submit lazy" ref="datesForm" @submit.prevent>
+                <v-card
+                    v-for="n in fieldsCount"
+                    class="pa-4"
+                    flat
+                >
+                    <div class="mt-2 mb-4">{{ `Важная дата ${n}`}}</div>
+                    <v-text-field
+                        class="pb-text-field rounded-lg"
+                        v-model="importantDates[`child${n}_name`]"
+                        variant="outlined"
+                        label="Название"
+                        density="compact"
+                        placeholder="Название даты или имя близкого"
+                        :rules="getRules(getFormFields.importantDaysName.rules)"
+                    ></v-text-field>
+                    <v-text-field
+                        class="pb-text-field"
+                        v-model="importantDates[`child${n}_birth_date`]"
+                        variant="outlined"
+                        label="Дата"
+                        density="compact"
+                        placeholder="дд.мм.гггг"
+                        :rules="getRules(getFormFields.birthDate.rules)"
+                    ></v-text-field>
+                    <v-card-actions v-if="n != 1">
+                        <VBtnOutline
+                            class="mt-2 pb-primary-bt"
+                            block
+                            size="small"
+                            color="#ffffff"
+                            variant="flat"
+                            @click="removeField(fieldsCount)"
+                            text="Удалить дату"
+                            :disabled="isMinFieldsCount"
+                        ></VBtnOutline>
+                    </v-card-actions>
+                </v-card>
+                <VBtn
+                    class="mt-2 pb-primary-bt"
+                    size="large"
+                    variant="flat"
+                    @click="addField"
+                    text="Добавить дату"
+                    :disabled="isMaxFieldsCount"
+                ></VBtn>
+                <VBtn
+                    class="mt-2 pb-primary-bt"
+                    block
+                    size="large"
+                    variant="flat"
+                    type="submit"
+                    :loading="loading"
+                    @click="sendImportantDatesForm"
+                    text="Сохранить"
+                ></VBtn>
+            </v-form>
+        </v-container>
+    </div>
 </template>
 <script>
 import {mapState, mapGetters, mapActions, mapMutations} from 'vuex'
