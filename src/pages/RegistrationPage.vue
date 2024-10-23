18be810d3ec6fabe4031b5a39e10240b02a4f249
Anton Beznos <abeznos@yandex.ru>
2024-10-23 04:01:14 +0300

    reg

--- src/pages/RegistrationPage.vue
+++ src/pages/RegistrationPage.vue
@@ -239,6 +239,7 @@ export default {
         async sendImportantDatesForm() {
             const response = await this.updateUserData({ref: this.$refs.datesForm, data: this.importantDates})
             if (response) {
+                alert(response)
                 console.log(response)
                 mainBtn.hide()
                 this.$router.go(-1)
